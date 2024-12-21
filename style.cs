5f280d416927f4fda7db61b7eab2fe9842133c27
ish-that-a-wish <ishikasainiakella@gmail.com>
2024-12-20 21:40:22 -0800

    Minor graphic changes

---
+++ style.cs
@@ -0,0 +1,8 @@
+@font-face {
+    font-family: 'Quicksand';
+    src: url('/Users/ishikasaini/Downloads/Quicksand/Quicksand-VariableFont_wght.ttf') format('truetype');
+}
+
+body {
+    font-family: 'Quicksand', sans-serif;
+}
